01bc53c65d355d3ad6c6de81ec85cb13f5279b8b
Nolan T Yoo <nolan@projectexist.net>
2017-11-20 03:33:56 -0800

    Add list shuffle

--- NTBUtils.cs
+++ NTBUtils.cs
@@ -125,6 +125,19 @@ public static class ListExtensions
   {
     ListExtensions.Resize(list, sz, default(T));
   }
+  
+  public static void UShuffle<T>(this IList<T> list)
+  {
+    int count = list.Count;
+    int last = count - 1;
+    for (int i = 0; i < last; ++i)
+    {
+      int r = UnityEngine.Random.Range(i, count);
+      T tmp = list[i];
+      list[i] = list[r];
+      list[r] = tmp;
+    }
+  }
 }
 public static class QuaternionExtensions
 {
