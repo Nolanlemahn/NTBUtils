8b915eca3c83cc3bbba621ed1d461cf4def7ef3c
Nolan T Yoo <nolan@projectexist.net>
2018-02-09 01:03:13 -0800

    Add total for platform-based auto removal

---
+++ RemoveByPlatform.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class RemoveByPlatform : MonoBehaviour
+{
+  public RuntimePlatform[] DeleteIf;
+  // Use this for initialization
+  void Start()
+  {
+    if (this.DeleteIf.Contains(Application.platform))
+    {
+      Object.Destroy(this.gameObject);
+    }
+  }
+
+}
