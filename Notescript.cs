2bf78329632de6c66ec25e34a1abe69d9c461abd
Nolan T Yoo <nolan@projectexist.net>
2018-02-15 04:19:56 -0800

    Add a mini note script (needs testing)

---
+++ Notescript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class Notescript : MonoBehaviour
+{
+  public string Note = "";
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(Notescript))]
+[CanEditMultipleObjects]
+class NotescriptGUI : Editor
+{
+  public override void OnInspectorGUI()
+  {
+    Notescript component = target as Notescript;
+
+    string oldNote = component.Note;
+    string newNote = GUILayout.TextArea(component.Note, int.MaxValue);
+
+    if (oldNote != newNote)
+    {
+      component.Note = newNote;
+      Undo.RecordObject(target, "Typing on Notescript on " + target.name + " [NTBU]");
+    }
+  }
+}
+#endif
